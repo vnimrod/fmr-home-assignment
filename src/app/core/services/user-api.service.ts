5f6dcbe6489fc73e079d87cdecb6011c4c1917a0
vnimrod <nimrod.weins@gmail.com>
2025-10-23 15:41:29 +0300

    add user api and user service

---
+++ src/app/core/services/user-api.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { delay, map, Observable, of } from 'rxjs';
+import { User } from '../../features/users/feature-state/users.models';
+import { USERS_MOCK } from '../mocks/users.mock';
+import { UserOrder } from '../../features/user-orders/feature-state/user-orders.models';
+import { ORDERS_MOCK } from '../mocks/orders.mock';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserApiService {
+  getUsers(): Observable<User[]> {
+    return of(USERS_MOCK).pipe(delay(500));
+  }
+
+  getUserById(id: string): Observable<User | undefined> {
+    return of(USERS_MOCK.find((user) => user.id === id)).pipe(delay(300));
+  }
+
+  getOrdersByUserId(userId: string): Observable<UserOrder[]> {
+    return of(ORDERS_MOCK).pipe(
+      delay(300),
+      map((orders) => orders.filter((order) => order.userId === userId))
+    );
+  }
+}
--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { switchMap, map } from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class UserServiceService {
+import type { User } from '../../features/users/feature-state/users.models';
+import { UserApiService } from './user-api.service';
+import { UserOrder } from '../../features/user-orders/feature-state/user-orders.models';
 
-  constructor() { }
+@Injectable({ providedIn: 'root' })
+export class UserService {
+  constructor(private readonly api: UserApiService) {}
+
+  getUsers$(): Observable<User[]> {
+    return this.api.getUsers();
+  }
+
+  getUserById$(id: string): Observable<User | undefined> {
+    return this.api.getUserById(id);
+  }
+
+  getUserOrders$(userId: string): Observable<UserOrder[]> {
+    return this.api.getOrdersByUserId(userId);
+  }
 }
